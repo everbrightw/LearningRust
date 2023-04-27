b4f820612c0f7a78b0d9e475baacda9981e87f8e
Yusen Wang <yusenw2@illinois.edu>
2023-04-27 22:46:35 +0800

    fix binary search, i suck

---
+++ leetcode_solutions/binary_search/target/debug/deps/binary_search-039ed83460d9277e.d
@@ -0,0 +1,5 @@
+/Users/yusenwang/rust_projects/LearningRust/leetcode_solutions/binary_search/target/debug/deps/binary_search-039ed83460d9277e.rmeta: src/main.rs
+
+/Users/yusenwang/rust_projects/LearningRust/leetcode_solutions/binary_search/target/debug/deps/binary_search-039ed83460d9277e.d: src/main.rs
+
+src/main.rs:
---
+++ leetcode_solutions/binary_search/target/debug/deps/binary_search-6ff637a29af9e80d.d
@@ -0,0 +1,5 @@
+/Users/yusenwang/rust_projects/LearningRust/leetcode_solutions/binary_search/target/debug/deps/binary_search-6ff637a29af9e80d.rmeta: src/main.rs
+
+/Users/yusenwang/rust_projects/LearningRust/leetcode_solutions/binary_search/target/debug/deps/binary_search-6ff637a29af9e80d.d: src/main.rs
+
+src/main.rs:
