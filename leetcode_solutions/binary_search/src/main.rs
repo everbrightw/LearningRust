4ec535bb125cc924eb5152da762c71c5ea07e6fd
Yusen Wang <yusenw2@illinois.edu>
2023-05-04 19:58:50 +0800

    matching pattern

--- leetcode_solutions/binary_search/src/main.rs
+++ leetcode_solutions/binary_search/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 fn main() {
     println!("hello binary search");
 
@@ -5,7 +7,7 @@ fn main() {
     let v: Vec<i32> = vec![-2, 0, 1, 2, 3, 4, 5, 100];
 
     //let ret = search(v, 4);
-    let ret2 = fuzzy_search(v, 100);
+    let ret2 = binary_search(v, 100);
 
     // print!("{ret}");
     print!("{ret2} \n");
@@ -31,7 +33,7 @@ pub fn search_with_iter(nums: Vec<i32>, target: i32) -> i32 {
     return -1;
 }
 
-pub fn fuzzy_search(nums: Vec<i32>, target: i32) -> i32 {
+pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
     let (mut left, mut right): (usize, usize) = (0, nums.len());
     while left < right {
         let mid: usize = (left + right) / 2;
@@ -44,4 +46,41 @@ pub fn fuzzy_search(nums: Vec<i32>, target: i32) -> i32 {
         }
     }
     return -1;
+}
+
+pub fn binary_search_matching_pattern(nums: &Vec<i32>, target: i32) -> i32 {
+    let (mut left, mut right) = (0, nums.len() - 1);
+
+    while left < right {
+        let mid = left + (right - left) / 2;
+        match nums[mid].cmp(&target) {
+            Ordering::Less => {
+                left = mid + 1;
+            }
+            Ordering::Greater => {
+                right = mid;
+            }
+            Ordering::Equal => {
+                return mid as i32;
+            }
+        }
+    }
+    -1
+}
+
+#[cfg(test)]
+
+mod test {
+    use crate::*;
+
+    #[test]
+    fn basic_test() {
+        let vec_test = vec![-1, 0, 1, 2, 3, 4, 5, 100];
+        assert_eq!(binary_search_matching_pattern(&vec_test, -1), 0);
+        assert_eq!(binary_search_matching_pattern(&vec_test, 0), 1);
+        assert_eq!(binary_search_matching_pattern(&vec_test, 1), 2);
+        assert_eq!(binary_search_matching_pattern(&vec_test, 2), 3);
+        assert_eq!(binary_search_matching_pattern(&vec_test, 3), 4);
+        assert_eq!(binary_search_matching_pattern(&vec_test, 101), -1);
+    }
 }
--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,9 +1,32 @@
 fn main() {
-    println!("Hello, world!");
-    let mut s = String::from("hello");
+    // println!("Hello, world!");
+    // let mut s = String::from("hello");
+    //
+    // change(&mut s);
+    // println!("string now is= {} ", s);
 
-    change(&mut s);
-    println!("string now is= {} ", s);
+    // learning matching pattern
+    let match_x = Some(10);
+    let match_y = 5;
+
+    match match_x {
+        Some(5) => println!("matched 5!!"),
+        Some(match_y) => println!("matched y!!!  y= {match_y}"),
+        _ => println!("matching anything, x={:?}", match_x),
+    }
+    println!("at the end: x = {:?}, y = {match_y}", match_x);
+
+    // destructure?
+    let p = Point { x: 0, y: 7 };
+    let Point { x, y } = p;
+    println!("=====================destructure===================");
+    println!("x = {x}, y ={y}");
+    println!("px= {}, py= {}", p.x, p.y);
+}
+
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 fn change(some_string: &mut String) {
