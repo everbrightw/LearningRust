75b907af067208ac4714c09f0faeecfafacf76e5
Yusen Wang <yusenw2@illinois.edu>
2023-05-15 19:42:49 +0800

    playground rust book ch.13

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -33,6 +33,40 @@ fn main() {
     let user2 = User { ..user1 };
 
     println!("user1 name = moved, user2 name = {}", user2.name);
+
+    // closure
+
+    let mut list = vec![1, 2, 3];
+    println!("Before closure: {:?}", list);
+    let mut test_closure = || list.push(4);
+    test_closure();
+    //push_a_vector(&mut list);
+
+    println!("After closure: {:?} \n", list);
+
+    println!("===== playing with iterators =====");
+    playing_with_iterators();
+
+    // Functional language features playground
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+
+    println!(
+        "===== userpref= {:?}, givingout= {:?}",
+        user_pref1,
+        store.giveaway(user_pref1)
+    );
+
+    let user_pref2 = None;
+
+    println!(
+        "===== userpref= {:?}, givingout= {:?}",
+        user_pref2,
+        store.giveaway(user_pref2)
+    );
 }
 
 struct Point {
@@ -60,4 +94,60 @@ fn first_world(s: &String) -> usize {
     }
 
     s.len()
+}
+
+fn push_a_vector(nums: &mut Vec<i32>) {
+    nums.push(4);
+}
+
+fn playing_with_iterators() {
+    let mut vec = vec![1, 2, 3, 4, 5];
+
+    for item in vec.iter() {
+        println!("{item}");
+    }
+
+    let v2: Vec<_> = vec.iter().map(|x| x + 1).collect();
+
+    println!("===== after map x+1 ====");
+
+    for item in v2.iter() {
+        println!("{item}");
+    }
+}
+
+// 13. Functional language Features
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
 }
